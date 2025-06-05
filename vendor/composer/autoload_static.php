0dbc112193b4471dd2f1376cd472262a481f6510
EugeneWinter <corvoattano200529@gmail.com>
2025-06-05 20:09:52 +0300

    Попытка №2

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -110,8 +110,8 @@ class ComposerStaticInit4616f429bf55bc2d73787bf2e227f57d
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-factory/src',
-            1 => __DIR__ . '/..' . '/psr/http-message/src',
+            0 => __DIR__ . '/..' . '/psr/http-message/src',
+            1 => __DIR__ . '/..' . '/psr/http-factory/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
