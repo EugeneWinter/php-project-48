a74aa1cfce3a5910c368b383fac50875faebf857
EugeneWinter <corvoattano200529@gmail.com>
2025-06-10 02:13:00 +0300

    86

--- src/Formatters/StylishFormatter.php
+++ src/Formatters/StylishFormatter.php
@@ -4,36 +4,38 @@ declare(strict_types=1);
 
 namespace Differ\Formatters\StylishFormatter;
 
-use Exception;
-
 function formatStylish(array $diff): string
 {
     $iter = function ($diff, $depth) use (&$iter) {
         $indent = str_repeat('    ', $depth);
         $lines = [];
-        
+
         foreach (sortByKey($diff) as $node) {
             switch ($node['type']) {
                 case 'nested':
                     $children = $iter($node['children'], $depth + 1);
-                    $lines[] = "{$indent}    {$node['key']}: {\n{$children}\n{$indent}    }";
+                    $lines[] = "{$indent}    {$node['key']}: {\n{$children}\n{$indent}}";
                     break;
+
                 case 'changed':
                     $lines[] = "{$indent}  - {$node['key']}: " . toString($node['oldValue'], $depth + 1);
                     $lines[] = "{$indent}  + {$node['key']}: " . toString($node['newValue'], $depth + 1);
                     break;
+
                 case 'added':
                     $lines[] = "{$indent}  + {$node['key']}: " . toString($node['value'], $depth + 1);
                     break;
+
                 case 'removed':
                     $lines[] = "{$indent}  - {$node['key']}: " . toString($node['value'], $depth + 1);
                     break;
+
                 case 'unchanged':
                     $lines[] = "{$indent}    {$node['key']}: " . toString($node['value'], $depth + 1);
                     break;
             }
         }
-        
+
         return implode("\n", $lines);
     };
 
@@ -55,16 +57,13 @@ function toString(mixed $value, int $depth = 1): string
     }
 
     $assoc = is_object($value) ? (array)$value : $value;
-
     ksort($assoc);
 
     $indent = str_repeat('    ', $depth);
     $bracketIndent = str_repeat('    ', $depth - 1);
 
     $lines = array_map(
-        function ($key) use ($assoc, $depth) {
-            return sprintf('%s%s: %s', str_repeat('    ', $depth), $key, toString($assoc[$key], $depth + 1));
-        },
+        fn($key) => sprintf('%s%s: %s', str_repeat('    ', $depth), $key, toString($assoc[$key], $depth + 1)),
         array_keys($assoc)
     );
 
