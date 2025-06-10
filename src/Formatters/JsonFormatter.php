b2d6f0036bf3e048a3320edd39b083391175b588
EugeneWinter <120814197+EugeneWinter@users.noreply.github.com>
2025-06-10 22:51:16 +0300

    Update JsonFormatter.php

--- src/Formatters/JsonFormatter.php
+++ src/Formatters/JsonFormatter.php
@@ -60,11 +60,17 @@ function prepareValueJson(mixed $value)
 {
     if (is_object($value)) {
         $assoc = (array) $value;
-        return sortAssocArray(array_map('prepareValueJson', $assoc));
+        return sortAssocArray(array_map(
+            fn($v) => prepareValueJson($v),
+            $assoc
+        ));
     }
 
     if (is_array($value)) {
-        return array_map('prepareValueJson', $value);
+        return array_map(
+            fn($v) => prepareValueJson($v),
+            $value
+        );
     }
 
     return $value;
@@ -77,58 +83,13 @@ function prepareValueJson(mixed $value)
 function sortAssocArray(array $array): array
 {
     $keys = array_keys($array);
-    $sortedKeys = mergeSort($keys, fn($a, $b) => strcmp((string)$a, (string)$b));
+    usort($keys, fn($a, $b) => strcmp((string)$a, (string)$b));
 
     return array_reduce(
-        $sortedKeys,
+        $keys,
         function (array $acc, $key) use ($array): array {
             return [...$acc, $key => $array[$key]];
         },
         []
     );
-}
-
-/**
- * @param array<mixed> $array
- * @param callable(mixed, mixed): int $comparator
- * @return array<mixed>
- */
-function mergeSort(array $array, callable $comparator): array
-{
-    if (count($array) <= 1) {
-        return $array;
-    }
-
-    $mid = (int)(count($array) / 2);
-    $left = array_slice($array, 0, $mid);
-    $right = array_slice($array, $mid);
-
-    return merge(
-        mergeSort($left, $comparator),
-        mergeSort($right, $comparator),
-        $comparator
-    );
-}
-
-/**
- * @param array<mixed> $left
- * @param array<mixed> $right
- * @param callable(mixed, mixed): int $comparator
- * @return array<mixed>
- */
-function merge(array $left, array $right, callable $comparator): array
-{
-    if (empty($left)) {
-        return $right;
-    }
-
-    if (empty($right)) {
-        return $left;
-    }
-
-    if ($comparator($left[0], $right[0]) <= 0) {
-        return [array_shift($left), ...merge($left, $right, $comparator)];
-    }
-
-    return [array_shift($right), ...merge($left, $right, $comparator)];
 }
