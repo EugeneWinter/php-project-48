5f5b60927934f3203a43df0a2c81deade040e91f
EugeneWinter <120814197+EugeneWinter@users.noreply.github.com>
2025-06-10 23:51:08 +0300

    Update Differ.php

--- src/Differ.php
+++ src/Differ.php
@@ -154,12 +154,9 @@ function prepareValue($value)
     if (is_object($value)) {
         $props = (array)$value;
         $result = new stdClass();
-        array_map(
-            function ($k) use ($props, &$result) {
-                $result->$k = prepareValue($props[$k]);
-            },
-            array_keys($props)
-        );
+        foreach (array_keys($props) as $k) {
+            $result->$k = prepareValue($props[$k]);
+        }
         return $result;
     }
     return $value;
