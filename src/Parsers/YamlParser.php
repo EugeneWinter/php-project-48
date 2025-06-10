9f983f29fcb20640963dcf8b5baaff72b5ffb100
EugeneWinter <120814197+EugeneWinter@users.noreply.github.com>
2025-06-10 22:25:04 +0300

    Update YamlParser.php

--- src/Parsers/YamlParser.php
+++ src/Parsers/YamlParser.php
@@ -24,9 +24,9 @@ function parse(string $content): stdClass
         if (!is_object($data)) {
             throw new Exception('YAML must represent an object');
         }
-        
-        return $data instanceof stdClass 
-            ? $data 
+
+        return $data instanceof stdClass
+            ? $data
             : (object) (array) $data;
     } catch (Exception $e) {
         throw new Exception("YAML parse error: {$e->getMessage()}");
